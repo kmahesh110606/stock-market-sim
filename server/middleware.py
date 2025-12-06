62a1b29541e6ea899ab20afa775473e8510e2fc8
V-Srivatsan <vatsan.programming@gmail.com>
2025-12-06 16:06:44 +0530

    Fixed env and db session

--- server/middleware.py
+++ server/middleware.py
@@ -7,12 +7,14 @@ import data.db as db
 from sqlalchemy.exc import NoResultFound
 from user.models import User
 
-def get_user(token: Annotated[str, Depends(OAuth2PasswordBearer("/login"))]):
+def get_user(
+    token: Annotated[str, Depends(OAuth2PasswordBearer("/login"))],
+    session: db.sql.Session = Depends(db.get_session)
+):
     try:
         data = jwt.decode(token, os.environ['SECRET'], algorithms=['HS256'])
-        with db.sql.Session(db.engine) as session:
-            res = session.exec(db.sql.select(User).where(User.uid == uuid.UUID(data['uid'])))
-            return res.one()
+        res = session.exec(db.sql.select(User).where(User.uid == uuid.UUID(data['uid'])))
+        return res.one()
 
     except (NoResultFound, jwt.InvalidTokenError):
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Credential validation failed")
